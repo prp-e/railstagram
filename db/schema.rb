042d664ce298f9f6f3e6c4988f2d03e32bb70569
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-08-19 10:33:56 +0430

    Like model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_14_203719) do
+ActiveRecord::Schema.define(version: 2020_08_19_060321) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "image"
@@ -33,4 +42,6 @@ ActiveRecord::Schema.define(version: 2020_08_14_203719) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
 end
