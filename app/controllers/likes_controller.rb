8b08d580f14665c6a5f1577367362ef659cb6d10
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-08-19 10:46:51 +0430

    Users can't like more than once

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,20 @@ class LikesController < ApplicationController
     before_action :find_post
 
     def create
-        @post.likes.create(:user_id => current_user.id)
-        redirect_to post_path(@post)
+        if already_liked?
+            flash[:notice] = "You already did that!"
+        else
+            @post.likes.create(:user_id => current_user.id)
+            redirect_to post_path(@post)
+        end 
     end 
 
     private
     def find_post
         @post = Post.find(params[:post_id])
+    end 
+
+    def already_liked?
+        Like.where(user_id: current_user.id, post_id: params[:post_id]).exists? 
     end 
 end
