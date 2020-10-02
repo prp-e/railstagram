4bd2ed059a4ddea9b5fec4eab7ee4a217985a248
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-10-02 20:41:24 +0330

    Now comments work like a charm

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -30,7 +30,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @post, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
