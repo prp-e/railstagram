2c26355e26cf8b32b71bbcbb9bec77c1d69b5ece
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-10-02 16:35:55 +0330

    Adding the comment feature

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
     mount_uploader :image, ImageUploader
     validates :caption, presence: true
     belongs_to :user
-    has_many :likes
+    has_many :likes, dependent: :destroy
+    has_many :comments, dependent: :destroy 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users
   resources :posts do 
     resources :likes 
