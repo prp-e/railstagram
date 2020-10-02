598a69561fa470cff332ef8975f1a7194d0df772
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-10-02 16:51:19 +0330

    New migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_130855) do
+ActiveRecord::Schema.define(version: 2020_10_02_132055) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "post_id"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
