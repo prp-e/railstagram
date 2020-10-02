3b732fbb8980bf76f8b417ddb4d5d57460c8ce9f
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-10-03 02:24:29 +0330

    Working on commenting functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -44,7 +44,7 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.html { redirect_to @post, notice: 'Comment was successfully updated.' }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit }
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users
   resources :posts do 
     resources :likes 
+    resources :comments
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
