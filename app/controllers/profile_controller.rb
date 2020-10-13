a6bfd0cfeb43ee0ce35739e8f2701d14c939f47c
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-10-13 21:02:28 +0330

    Profile backend is ready

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,8 +1,10 @@
 class ProfileController < ApplicationController
     def show
+        @posts = @user.posts
     end 
 
     private
     def find_user
+        @user = User.find_by(username: params[:username])
     end 
 end
