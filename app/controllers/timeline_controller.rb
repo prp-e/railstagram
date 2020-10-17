bffd41b5dd7b07f8ec52107ef37fd38e816efb5a
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-10-18 03:00:12 +0330

    Controller for timeline finished

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -1,2 +1,10 @@
 class TimelineController < ApplicationController
+    def index
+        @followings = @user.favorites.find_all 
+    end 
+
+    private
+    def find_user
+        @user = User.find(current_user.id)
+    end 
 end
