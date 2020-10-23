9bdd0303ffdc926ab766ad45585b764390fd5e19
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-10-24 02:10:54 +0330

    A root index was needed

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
     resources :likes 
     resources :comments
   end 
+
+  root  "timeline#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
